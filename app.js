9cf97c36ca9f62315a6898f8eb9c62fb607e5565
VinayJyothi <105520754+VinayJyothi@users.noreply.github.com>
2023-11-01 12:38:30 -0500

    updated

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bankRouter = require('./routes/bank');
 var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/bank', bankRouter);
 app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
